4f67c35ae7bf975c1056b0c11dcc20a0da0f175e
ShadVlad <wowchik.shadrin@yandex.ru>
2020-03-22 20:39:34 +0300

    create

---
+++ src/main/java/koschei/models/Deth8.java
@@ -0,0 +1,13 @@
+package koschei.models;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Deth8 {
+
+    @Override
+    public String toString() {
+        return ":( ";
+    }
+}
